51280352d3aa964bc760589ce98292553d14ec3e
AdrianaV1999 <75784049+AdrianaV1999@users.noreply.github.com>
2026-01-04 21:18:55 +0100

    Header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img src="/cook-book.png" alt="logo"></img>
+      <h1>Recipely</h1>
+    </header>
+  );
+}
+
+export default Header;
